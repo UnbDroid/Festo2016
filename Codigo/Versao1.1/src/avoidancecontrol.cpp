e805d7332ca16c3e5293463b403ebbe56e009b15
Camila Brito <camilabrito@paulo-Positivo-Mobile.(none)>
2016-08-23 21:08:28 -0300

    Banana

---
+++ Codigo/Versao1.1/src/avoidancecontrol.cpp
@@ -0,0 +1,64 @@
+#include "avoidancecontrol.hpp"
+#include "robotino.hpp"
+#include "Classificadores.hpp"
+#include "maquinainferencia.hpp"
+#include "defuzzyficador.hpp"
+#include "irparaponto.hpp"
+#include <vector>
+#include <cmath>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#define PI 3.14159265
+//*****************************************************************************************************************
+// AvoidanceControl
+//*****************************************************************************************************************
+
+AvoidanceControl  *AvoidanceControl::instance()
+{
+    static AvoidanceControl  AvoidanceControlStateInstance;
+    return &AvoidanceControlStateInstance;
+}
+
+AvoidanceControl::AvoidanceControl() {}
+
+AvoidanceControl::~AvoidanceControl() {}
+
+void AvoidanceControl::enter(Robotino *robotino)
+{
+    //if(!c_AvoidanceControl01) throw std::invalid_argument("AvoidanceControl must have an owner to enter.");
+    std::cout << "Entrando no avoidance control...\n";
+    //robotino->omniDrive.setVelocity(-100, 0 , 0 );
+}
+
+float corrigirAngulo(float Angulo){
+    if(Angulo < -180)
+        return Angulo+360;
+    if(Angulo > 180)
+        return Angulo-360;
+    return Angulo;
+}
+
+void AvoidanceControl::execute(Robotino *robotino)
+{
+    //robotino->definirDestino(0,100);
+    //robotino->change_state(IrParaPonto::instance());
+    robotino->definirDestino(-10,0);
+    robotino->change_state(IrParaPonto::instance());
+    //robotino->update();
+    robotino->exit("Terminou essa porra!");
+    //std::cout << test << "\n";
+    //std::cout << "Vendo na direita: " <<robotino->readColorSensor(Robotino::SC_DIREITO) << "\n";
+    //std::cout << "Vendo na esquerda: " <<robotino->readColorSensor(Robotino::SC_ESQUERDO) << "\n";
+    //std::cout << "Vendo no indutivo: " <<robotino->readInductiveSensor() << "\n";
+    //cv::Mat img = robotino->getImage(); 
+    //cv::Mat teste (img,cv::Rect(0,0,img.cols,img.rows - 10));
+    //std::cout << img;
+    //cv::imshow("Amor",img);
+    //cv::waitKey(1);
+    //std::cout << robotino->getImage();
+}
+
+void AvoidanceControl::exit(Robotino *robotino) {
+    std::cout << "Saindo no avoidance control...\n";
+}
---
+++ Codigo/Versao1.1/src/irparaponto.cpp
@@ -0,0 +1,73 @@
+#include "irparaponto.hpp"
+#include "robotino.hpp"
+#include "Classificadores.hpp"
+#include "maquinainferencia.hpp"
+#include "defuzzyficador.hpp"
+#include <vector>
+#include <cmath>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#define PI 3.14159265
+
+//*****************************************************************************************************************
+// IrParaPonto
+//*****************************************************************************************************************
+
+IrParaPonto  *IrParaPonto::instance()
+{
+    static IrParaPonto  IrParaPontoStateInstance;
+    return &IrParaPontoStateInstance;
+}
+
+IrParaPonto::IrParaPonto() {}
+
+IrParaPonto::~IrParaPonto() {}
+
+void IrParaPonto::enter(Robotino *robotino)
+{
+    std::cout << "Indo para o ponto...\n";
+}
+
+void IrParaPonto::execute(Robotino *robotino)
+{
+
+    std::cout << "Running IrParaPonto...\n";
+    ClassificadorAngulo CA;
+    ClassificadorDistancia CD;
+    Defuzzyficador D;
+    MaquinaInferencia MI;
+    static std::vector<float> x,y,theta_d,dist_d,vx,vy;
+
+    // Calculando a distancia e o angulo para o alvo
+    robotino->d_e = robotino->calc_dist(robotino->x_d,robotino->odometryX(),robotino->y_d,robotino->odometryY())/10;
+    robotino->theta_e = -atan2(robotino->y_d-robotino->odometryY(),robotino->x_d-robotino->odometryX())*180/PI;
+
+    // Fuzzificando os valores de angulo e distancia desejados
+    CA.classificar(robotino->theta_e);
+    CD.classificar(robotino->d_e);
+    vector<float> pertinenciaAngulo = CA.pegarVetorPertinencia();
+    vector<float> pertinenciaDistancia = CD.pegarVetorPertinencia();
+
+    // Gerando regras para os valores com a Maquina de Inferencia
+    vector<float> saidaVx = MI.tabelaRegrasVx(pertinenciaDistancia,pertinenciaAngulo);
+    vector<float> saidaVy = MI.tabelaRegrasVy(pertinenciaDistancia,pertinenciaAngulo);
+
+    //  Defuzzificando os valores
+    float Vx = D.centroDeMassa(saidaVx);
+    float Vy = D.centroDeMassa(saidaVy);
+
+    // Falando para o robo as velocidades que ele deve se mover
+    robotino->setVelocity(Vx,Vy,0);
+
+    // Se tiver chegado ao alvo, voltar para o estado anterior
+    std::cout << "D_E "<<robotino->d_e<<"...\n";
+    if(robotino->d_e < 20){
+        robotino->change_state(robotino->previous_state());
+    }
+}
+
+void IrParaPonto::exit(Robotino *robotino) {
+    std::cout << "Cheguei no ponto...\n";   
+    robotino->setVelocity(0,0,0);    
+}
