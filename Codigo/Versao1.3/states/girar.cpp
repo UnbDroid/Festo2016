b3e4e450e7bb999f00c4b4eabc8079757061f2e3
Camila Brito <camilabrito@paulo-Positivo-Mobile.(none)>
2016-09-02 18:54:09 -0300

    Mudando

--- Codigo/Versao1.3/states/girar.cpp
+++ Codigo/Versao1.3/states/girar.cpp
@@ -8,6 +8,8 @@
 
 #define Kp 1.91
 #define Ki 1
+#define Kp2 0.5
+#define Ki2 0.3
 #define limiar 5
 #define dt 0.01
 //*****************************************************************************************************************
@@ -31,19 +33,34 @@ void Girar::execute(Robotino *robotino){
     // Fazer o controlador para o robô se manter no thetaR
     float w, erro = (robotino->odometryPhi() - robotino->thetaR);
     static float erro_int = 0;
-    std::cout << "Phi: " << robotino->odometryPhi() << "\n";
-    std::cout << "Referencia: "<< robotino->thetaR << "\n";
-    std::cout << "Erro: " << erro << "\n";
-    std::cout << "Dt: " << dt <<"\n";
-    erro_int += erro*dt;
-    w = -Kp*erro-Ki*erro_int;
-    robotino->setVelocity(0,0,w);
+    
+    if(robotino->carregandoDisco()){
+        std::cout << "Phi: " << robotino->odometryPhi() << "\n";
+        std::cout << "Referencia: "<< robotino->thetaR << "\n";
+        std::cout << "Erro: " << erro << "\n";
+        std::cout << "Dt: " << dt <<"\n";
+        erro_int += erro*dt;
+        w = -Kp2*erro-Ki2*erro_int;
+        robotino->setVelocity(0,0,w);
 
-    if (std::abs(erro) < limiar){
-             erro_int = 0;
-             robotino->change_state(robotino->previous_state());
+        if (std::abs(erro) < limiar){
+                 erro_int = 0;
+                 robotino->change_state(robotino->previous_state());
+        }
+    }else{
+        std::cout << "Phi: " << robotino->odometryPhi() << "\n";
+        std::cout << "Referencia: "<< robotino->thetaR << "\n";
+        std::cout << "Erro: " << erro << "\n";
+        std::cout << "Dt: " << dt <<"\n";
+        erro_int += erro*dt;
+        w = -Kp*erro-Ki*erro_int;
+        robotino->setVelocity(0,0,w);
+
+        if (std::abs(erro) < limiar){
+                 erro_int = 0;
+                 robotino->change_state(robotino->previous_state());
+        }
     }
-
 }
 
 void Girar::exit(Robotino *robotino) {
